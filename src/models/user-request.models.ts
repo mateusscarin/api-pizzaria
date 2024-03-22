25e2b301eb2ff185214c479ff43d67a1c51f2b79
mateusscarin <mateus.scarin@gmail.com>
2024-03-22 20:13:02 -0300

    feat: método de Cadastro de User

---
+++ src/models/user-request.models.ts
@@ -0,0 +1,5 @@
+export interface UserRequest {
+    nome: string;
+    email: string;
+    senha: string;
+}
---
+++ src/services/user/CreateUserService.ts
@@ -0,0 +1,23 @@
+import { UserRequest } from "../../models/user-request.models";
+import prismaClient from "../../prisma";
+
+class CreateUserService {
+    async execute({ nome, email, senha }: UserRequest) {
+        if (!email) {
+            throw new Error("E-mail não informado!");
+        }
+
+        const userAlreadyExists = await prismaClient.usuario.findFirst({ where: { email: email } });
+        if (userAlreadyExists) {
+            throw new Error("E-mail informado já está cadastrado!");
+        }
+        const user = await prismaClient.usuario.create({
+            data: { email: email, nome: nome, senha: senha },
+            select: { id: true, nome: true, email: true, criado_em: true }
+        })
+        return user;
+    }
+}
+
+export { CreateUserService };
+
